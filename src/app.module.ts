855c4c3fac2d42adf1f33d6e9d49b061e2e604d4
Sammwy <sammwy.dev@gmail.com>
2021-07-21 22:01:17 -0300

    Added MongoDB Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot(),
+      MongooseModule.forRoot(process.env.MONGO_URI, {
+          useCreateIndex: true,
+          useFindAndModify: true,
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
