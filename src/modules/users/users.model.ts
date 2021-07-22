ebdeeaa20409a972ea5f672e12bcc3dc9b621513
Sammwy <sammwy.dev@gmail.com>
2021-07-22 19:31:22 -0300

    Added method in user model to compare password

--- src/modules/users/users.model.ts
+++ src/modules/users/users.model.ts
@@ -1,7 +1,7 @@
 import * as bcrypt from "bcrypt";
 import { Field, ID, ObjectType } from "@nestjs/graphql";
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HookNextFunction } from "mongoose";
+import { HookNextFunction, Document } from "mongoose";
 
 @ObjectType()
 @Schema()
@@ -19,20 +19,35 @@ export class User {
 
     @Prop({ required: true, unique: true, trim: true, lowercase: true })
     email: string;
+
+    @Prop({ required: true })
+    password: string;
 }
 
 export type UserDocument = User & Document;
 export const UserSchema = SchemaFactory.createForClass(User);
 
-UserSchema.pre('save', async function (next: HookNextFunction) {
-    try {
-        if (this.isModified('password')) {
-            const hashed = await bcrypt.hash(this['password'], 10);
-            this['password'] = hashed;
-        }
 
-        next();
-    } catch (e) {
-        return next(e);
+UserSchema.pre('save', async function (next: HookNextFunction) {
+    let user = this as UserDocument;
+
+    // Check if the password field is modified.
+    if (!user.isModified('password')) {
+        // Else, return.
+        return next();
     }
+
+    // Generate salt and hash the user password with it.
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hashSync(user.password, salt);
+
+    // Replace the password with the hash.
+    user.password = hash;
+
+    return next();
 });
+
+UserSchema.methods.comparePassword = async function (passwordCandidate: string) {
+    const user = this as UserDocument;
+    return bcrypt.compare(passwordCandidate, user.password)
+}
